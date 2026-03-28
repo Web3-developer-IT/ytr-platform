b072e0565f8854a06a9c4a6cb8d7a660b6591ac6
josh <1joshuamomo@gmail.com>
2026-03-28 18:02:40 +0000

    switch to sqlite for render deploy

--- yourstorent_project/settings.py
+++ yourstorent_project/settings.py
@@ -92,12 +92,8 @@ WSGI_APPLICATION = 'yourstorent_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'yourstorent',
-        'USER': 'postgres',
-        'PASSWORD': '1LoveGod12#',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
